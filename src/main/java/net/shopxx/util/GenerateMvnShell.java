d3e87d47270dc4f389840763b45ee34fbb203139
Cisa <cisasoft@outlook.com>
2016-03-20 01:23:49 +0800

    before install

--- src/main/java/net/shopxx/util/GenerateMvnShell.java
+++ src/main/java/net/shopxx/util/GenerateMvnShell.java
@@ -3,13 +3,18 @@ package net.shopxx.util;
 import java.io.File;
 
 /**
- * 本工具目的是将非mvn项目转换成maven管理项目
- * 使用方法，将jar包放入对应路径，生成call mvn的Windows批处理命令，放入Windows中执行
- * 第二步，将生成pom的引用dependency脚本复制入
+ * 本工具目的是将非mvn项目转换成maven管理项目 使用方法，将jar包放入对应路径，生成call
+ * mvn的Windows批处理命令，放入Windows中执行 第二步，将生成pom的引用dependency脚本复制入
+ * 
  * @author Daniel
- *
+ * 
  */
 public class GenerateMvnShell {
+
+	public static void main(String[] args) {
+		String path = "C:/Users/Niexl/Workspaces/MyEclipse for Spring 2014/myshop/src/main/webapp/WEB-INF/lib";// jar包的存放路径
+		ListFile(path);
+	}
 
 	public static void ListFile(String path) {
 		File file = new File(path);
@@ -26,33 +31,28 @@ public class GenerateMvnShell {
 					} else {
 						// System.out.println("文件:" + file2.getAbsolutePath());
 
-//						String cmd = "call mvn install:install-file -Dfile="
-//								+ file2.getAbsolutePath()
-//								+ " -DgroupId=net.shopxx -DartifactId="
-//								+ file2.getName().substring(0,
-//										file2.getName().lastIndexOf(".jar"))
-//								+ " -Dversion=3.0 -Dpackaging=jar;";
-//						System.out.println(cmd);
-
-						String pom = "<dependency>\n"
-								+ " <groupId>net.shopxx</groupId>\n"
-								+ " <artifactId>"
+						String cmd = "call mvn install:install-file \"-Dfile="
+								+ file2.getAbsolutePath()
+								+ "\" \"-DgroupId=net.shopxx\" \"-DartifactId="
 								+ file2.getName().substring(0,
 										file2.getName().lastIndexOf(".jar"))
-								+ "</artifactId>\n"
-								+ " <version>3.0</version>\n" + "</dependency>";
-						System.out.println(pom);
+								+ "\" \"-Dversion=3.0\" \"-Dpackaging=jar\"";
+						System.out.println(cmd);
+
+						// String pom = "<dependency>\n"
+						// + " <groupId>net.shopxx</groupId>\n"
+						// + " <artifactId>"
+						// + file2.getName().substring(0,
+						// file2.getName().lastIndexOf(".jar"))
+						// + "</artifactId>\n"
+						// + " <version>3.0</version>\n" + "</dependency>";
+						// System.out.println(pom);
 					}
 				}
 			}
 		} else {
 			System.out.println("文件不存在!");
 		}
-	}
-
-	public static void main(String[] args) {
-		String path = "C:/Users/Daniel/Downloads/shopxx-3.0RELEASE/WebContent/WEB-INF/lib";// jar包的存放路径
-		ListFile(path);
 	}
 
 }
